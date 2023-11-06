afd930aeb3fd6a515e21125d0f09fd7d67a694a9
Beatriz Oliveira Ferreira <beaferreira.trabalho@gmail.com>
2023-11-06 15:26:23 -0300

    Add project files.

---
+++ ExerciciosProposos02/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Globalization;
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program {
+        static void Main(string[] args) {
+
+            double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double pi = 3.14159;
+            double formulaDePi = pi * Math.Pow(raio, 2);
+
+            Console.WriteLine("A = " + formulaDePi.ToString("F4", CultureInfo.InvariantCulture));
+        }
+    }
+}
